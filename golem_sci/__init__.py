0942e88ff2a318ed34b6608091efe766dc02571e
Adam Mizerski <amizerski@golem.network>
2018-03-12 12:48:42 +0100

    check for secp256k1.HAS_RECOVERABLE
    https://github.com/golemfactory/golem/issues/2168

--- golem_sci/__init__.py
+++ golem_sci/__init__.py
@@ -1,3 +1,25 @@
+def _check_secp256k1_recovery():
+    """
+    secp256k1 library has optional "ECDSA pubkey recovery module", which is
+    not built by default (requires ./configure --enable-module-recovery).
+
+    secp256k1-py python module in setup.py searches for the secp256k1 library
+    and uses it. If it's not installed, it builds one by itself, with pubkey
+    recovery module enabled.
+
+    Ethereum python module requires recovery module enabled in secp256k1-py.
+    """
+
+    import secp256k1
+    if not secp256k1.HAS_RECOVERABLE:
+        raise NotImplementedError(
+            "secp256k1 is built without recovery module. "
+            "See https://github.com/golemfactory/golem/issues/2168")
+
+
+_check_secp256k1_recovery()
+
+
 from .factory import (  # noqa
     new_sci,
     new_sci_ipc,
