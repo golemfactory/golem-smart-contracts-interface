b5642b62129653a2bd46f114609265bf6687e4cb
Igor Adamski <igor@golem.network>
2018-03-20 16:05:34 +0100

    Update abi_bin script

--- scripts/abi_bin.py
+++ scripts/abi_bin.py
@@ -1,7 +1,7 @@
 import json
 import click
 
-CONTRACTS = [
+TESTNET_CONTRACTS = [
     'GolemNetworkToken',
     'GolemNetworkTokenBatching',
     'GNTPaymentChannels',
@@ -9,31 +9,40 @@ CONTRACTS = [
     'Faucet',
 ]
 
+MAINNET_CONTRACTS = [
+    'GolemNetworkTokenBatching',
+]
+
 TEMPLATE = "\
-class {}:\n\
-    ADDRESS = ''\n\
-    ABI = '{}'  # noqa\n\
-    BIN = '{}'  # noqa\n\
+ADDRESS = ''\n\
+ABI = '{}'  # noqa\n\
+BIN = '{}'  # noqa\n\
 "
 
 
 @click.command()
 @click.option(
     "--filename",
+    required=True,
     help="JSON file generated using `solc contracts/* --combined-json abi,bin`")
 @click.option(
     "--output_dir",
     help="Where to save output (or stdout if not provided)")
-def main(filename, output_dir):
+@click.option(
+    "--mainnet",
+    is_flag=True,
+    default=False,
+    help="Mainnet if provided, rinkeby otherwise")
+def main(filename, output_dir, mainnet):
     with open(filename, 'r') as f:
         data = json.load(f)
     contracts = {
         key.split('/')[-1]: value for key, value in data['contracts'].items()
     }
-    for contract_name in CONTRACTS:
+    contract_names = MAINNET_CONTRACTS if mainnet else TESTNET_CONTRACTS
+    for contract_name in contract_names:
         contract = contracts['{0}.sol:{0}'.format(contract_name)]
         content = TEMPLATE.format(
-            contract_name,
             contract['abi'],
             contract['bin'],
         )
