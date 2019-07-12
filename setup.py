2379c1c250cb672f11a1428098d73c37e65d90ad
Igor Adamski <igor@golem.network>
2019-07-12 14:57:57 +0200

    Bump to v1.10.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Golem-Smart-Contracts-Interface',
-    version='1.10.0',
+    version='1.10.1',
     url='https://github.com/golemfactory/golem-smart-contracts-interface',
     maintainer='The Golem team',
     maintainer_email='tech@golem.network',
